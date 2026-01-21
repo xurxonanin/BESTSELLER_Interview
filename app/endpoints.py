75599e3cdb635fcd891cc9936da8dbae21672274
Xurxo Nanín <xunanin@duck.com>
2026-01-21 12:13:50 +0100

    Added logging to API calls

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field
 from pathlib import Path
+import logging
 import joblib
 import pandas as pd
 
+logger = logging.getLogger("uvicorn.error")
 MODEL_PATH = Path("models/price_prediction_elasticnet.joblib")
 COLS = ["X1 transaction date", "X2 house age", "X3 distance to the nearest MRT station", "X4 number of convenience stores", "X5 latitude", "X6 longitude"]
 models = {}
@@ -19,19 +21,24 @@ class PredictRequest(BaseModel):
 # We load the model on startup, in case of error raise exception
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logger.info("Launching application. Loading models...")
     if not MODEL_PATH.exists():
+        logger.error("Model file not found. Stopping.")
         raise RuntimeError(f"Model file not found in {MODEL_PATH.resolve()}")
     app.state.model = joblib.load(MODEL_PATH)
+    logger.info("Model loaded.")
     yield
 
 app = FastAPI(lifespan=lifespan)
 
 @app.get("/health")
 async def health():
+    logger.info("Health request received.")
     return{"status": "OK"}
 
 @app.post("/predict")
 async def get_prediction(input: PredictRequest):
+    logger.info("Price prediction called.")
     rows = {
         "X1 transaction date":input.transaction_date,
         "X2 house age":input.house_age,
@@ -41,5 +48,7 @@ async def get_prediction(input: PredictRequest):
         "X6 longitude":input.longitude
     }
     features = pd.DataFrame([rows], columns = COLS)
+    logger.info("Values loaded for prediction.")
     pred = app.state.model.predict(features)
+    logger.info("Returning prediction")
     return{"predicted_price": f"{float(pred[0]):.2f}"}
